b8353260e10c0a9c201cc881ff3c264be3228cb5
andres <andshb69@gmail.com>
2019-09-07 09:43:42 -0500

    cv
    com

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +7,45 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  fff="";
 
-  constructor() {}
 
+  public nombre:string;
+ public name: string;
+
+  
+
+  constructor(public toastController: ToastController) {
+
+
+    this.nombre = ""; 
+   
+
+  }
+
+  save()
+  {
+  var str = this.fff;
+  //alert(str);
+  var spl = str.split(",");
+  var res = spl[Math.floor(Math.random() * (spl.length - 0) + 0)];
+  this.nombre =res;
+
+  this.presentToast(res) 
+  }
+
+
+  async presentToast(text:any) {
+    const toast = await this.toastController.create({
+      message: text,
+      duration: 2000
+    });
+    toast.present();
+  }
+
+
+
+  private newMethod(): string {
+    return 'entrada';
+  }
 }
